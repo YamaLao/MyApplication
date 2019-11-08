8fc731c05461c74a011513f32c31830d786934f5
YamaLao <mountain1128@gmail.com>
2019-11-08 16:06:20 +0800

    123

--- app/src/main/java/jon/com/myapplication/MainActivity.java
+++ app/src/main/java/jon/com/myapplication/MainActivity.java
@@ -1,23 +1,40 @@
 package jon.com.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+//private ImageView IV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViews();
-        secondImage.setImageResource(R.drawable.ChineseYear-01);
-    }
+//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//        imageView.setImageResource(R.drawable.下載);
 
-    private ImageView secondImage;
+        //Glide.with(this).load(R.drawable.member_0004).into(imageView);
+//IV=(ImageView)findViewById(R.id.imageView) ;
+       // findViews();
+//IV.setImageResource(R.drawable.ppp);
+    //   IV.setImageResource(R.drawable.下載);able
+        val animator = ApngAnimator(context).loadInto(imageView)
+        animator.load("https://apng.onevcat.com/assets/elephant.gif")
+        // Load a apng to imageView
+        // ImageView scaleType support CENTER_INSIDE/CENTER_CROP/FIT_XY, and default FIT_XY
 
-    private void findViews() {
-        secondImage = (ImageView) findViewById(R.id.imageView);
-    }
+
+
+    //private ImageView secondImage;
+
+//    private void findViews() {
+//        IV = (ImageView) findViewById(R.id.imageView);
+//    }
 }
